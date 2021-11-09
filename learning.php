d8f837e81e6eee9014b951d49035b9842cb7ae3f
lokigt121 <dmitrylos14@gmail.com>
2021-11-09 10:43:01 +0300

    Детальнее осваиваю гит

--- learning.php
+++ learning.php
@@ -13,10 +13,10 @@ class ShopProduct{
 //Листинг 03.02
 $product1 = new ShopProduct();
 print $product1->getProducer();
-//$product2 = new ShopProduct();
+$product2 = new ShopProduct();
 //$product1->title = "Собачье сердце";
 //$product2->title = "Ревизор";
 //print $product1->title;
 //print $product2->title;
-//var_dump($product1);
-//var_dump($product2);
+var_dump($product1);
+var_dump($product2);
