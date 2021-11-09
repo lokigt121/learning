7906a6d884bbbbdfcce226266ef5ddcef1709ce3
lokigt121 <dmitrylos14@gmail.com>
2021-11-09 10:52:44 +0300

    Вызов метода

--- learning.php
+++ learning.php
@@ -2,8 +2,8 @@
 // Листинг 03.01
 class ShopProduct{
     public $title = "Стандартный товар";
-    public $producerMainName = "Лось";
-    public $producerFirstName = "Дмитрий";
+    public $producerMainName = "Фамилия автора";
+    public $producerFirstName = "Имя автора";
     public $price = 0;
     public function getProducer() {
         return $this->producerFirstName . " " . $this->producerMainName;
@@ -12,11 +12,17 @@ class ShopProduct{
 
 //Листинг 03.02
 $product1 = new ShopProduct();
-print $product1->getProducer();
-$product2 = new ShopProduct();
+$product1->title = "Обучение";
+$product1->producerMainName = "Лось";
+$product1->producerFirstName = "Дмитрий";
+$product1->price = "6";
+print "Товар: $product1->title <br> Автор: {$product1->getProducer()} ";
+
+//print $product1->getProducer();
+//$product2 = new ShopProduct();
 //$product1->title = "Собачье сердце";
 //$product2->title = "Ревизор";
 //print $product1->title;
 //print $product2->title;
-var_dump($product1);
-var_dump($product2);
+//var_dump($product1);
+//var_dump($product2);
